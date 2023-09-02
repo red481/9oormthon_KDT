5cb8cab8d71d4bacefd800b2235940b305db798c
Dong Soo <rsy48@naver.com>
2023-09-02 14:56:18 +0900

    completed

--- PBL_assignment03/main.js
+++ PBL_assignment03/main.js
@@ -11,6 +11,7 @@ class GithubStatusManagement {
     this.createdDate = '';
     this.location = '';
     this.blog = '';
+    this.userRepos = '';
   }
 
   loadHTML(username) {
@@ -25,6 +26,11 @@ class GithubStatusManagement {
       })
       .then(data => {
         this.userStatus = data;
+        this.updateProfile();
+        
+      })
+      .then(()=>{
+        this.loadLatestRepos();
       })
       .catch(error => {
         console.log('Fetch error: ', error.message);
@@ -32,16 +38,74 @@ class GithubStatusManagement {
       
   }
 
+  loadLatestRepos(){
+    const URL = this.userStatus.repos_url;
+    fetch(URL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok!!');
+        }        
+        response.json().then(result => {
+          this.userRepos = result;
+          this.parseUserReposAndPrint();
+        });
+      })
+      .catch(error => {
+        console.log('Fetch error: ', error.message);
+      });
+  }
   
+  parseUserReposAndPrint = function(){
+    const containerTag = document.querySelector('.latest_repos');
+    let insertTag = `<div class="section">
+    <div class="repo_name"><p>react-disney-deploy-test</p></div>
+    <div class="additory_information">
+      <div class="detail_of_additory_information" id="star">Stars: </div>
+      <div class="detail_of_additory_information" id="whatcher">Whatchers: </div>
+      <div class="detail_of_additory_information" id="fork">Forks: </div>
+    </div>
+  </div>`;
+    if(this.userRepos.length < 1){
+      return;
+    }
+    this.userRepos.forEach((repo, idx, array)=>{
+      containerTag.insertAdjacentHTML('beforeend', insertTag);
+      let repoSection = containerTag.lastElementChild;
+      repoSection.children[0].innerText = repo.name;
+      let tagToEdit = repoSection.children[1];
+      
+      let divElement = tagToEdit.children[0];
+        divElement.innerText = `Stars: ${repo.stargazers_count}`;
+        divElement = tagToEdit.children[1];
+        divElement.innerText = `Whatchers: ${repo.watchers}`;
+        divElement = tagToEdit.children[2];
+        divElement.innerText = `Forks: ${repo.forks}`;
+      });
+  }
+
 updateProfile = function(){
   const obj = this.userStatus;
   let element = document.getElementById('public_repos');
-  console.log(element);
-  element.innerText = obj.public_repos;
+  element.innerText = `public repos : ${obj.public_repos}`;
   element = document.getElementById('public_gists');
-  element.innerText = obj.public_gists;
-
+  element.innerText = `public gists : ${obj.public_gists}`;
+  element = document.getElementById('followers');
+  element.innerText = `followers : ${obj.followers}`;
+  element = document.getElementById('following');
+  element.innerText = `following : ${obj.following}`;
+  element = document.getElementById('company');
+  element.innerText = `company : ${obj.company}`;
+  element = document.getElementById('website_blog');
+  element.innerText = `website/blog : ${obj.blog}`;
+  element = document.getElementById('location');
+  element.innerText = `location : ${obj.location}`;
+  element = document.getElementById('member_since');
+  element.innerText = `member since : ${obj.created_at}`;
+  element = document.querySelector('.profile_image');
+  element.innerHTML = `<img src=${obj.avatar_url} alt="profileIMG">`
 }
+
+
   // parseUserStatus(userStatus){
   //   this.company = userStatus.company;
   //   this.repos = userStatus.public_repos;
