ac432e4555d243358fc3e24feae9d66b65b1984a
Dong Soo <rsy48@naver.com>
2023-09-05 15:44:14 +0900

    complete

--- PBL_assignment04/main.js
+++ PBL_assignment04/main.js
@@ -1,12 +1,13 @@
-document.querySelectorAll('.row-name').forEach(function(element) {
-  element.setAttribute('contenteditable', 'false');
-});
-
 document.getElementById('editableTable').addEventListener('click', function(event) {
-  if (event.target.tagName === 'TD') {
+  if (event.target.tagName === 'TD' && !(event.target.classList.contains('row-name')))
+ {
       event.target.setAttribute('contenteditable', 'true');
       event.target.focus();
   }
+});
+
+document.querySelectorAll('.row-name').forEach(function(element) {
+  element.setAttribute('contenteditable', 'false');
 });
 
 document.addEventListener('click', function(event) {
@@ -27,7 +28,7 @@ document.querySelectorAll('.td-cell').forEach(function(cell) {
       let colIndex = Array.prototype.indexOf.call(row.children, cell);
       let table = document.getElementById('editableTable');
       let currentCell = document.getElementById('current_cell').firstElementChild;
-      currentCell.innerText = `Cell : ${rowIndex + 1}${table.rows[0].cells[colIndex].textContent}`;
+      currentCell.innerText = `Cell : ${table.rows[0].cells[colIndex].textContent}${rowIndex + 1}`;
 
       let rowNameTag = table.rows[rowIndex + 1].cells[0];
       let colNameTag = table.rows[0].cells[colIndex];
@@ -43,15 +44,23 @@ document.querySelectorAll('.td-cell').forEach(function(cell) {
       let rowIndex = Array.prototype.indexOf.call(row.parentNode.children, row);
       let colIndex = Array.prototype.indexOf.call(row.children, cell);
       let table = document.getElementById('editableTable');
-      
-      
 
       let rowNameTag = table.rows[rowIndex + 1].cells[0];
       let colNameTag = table.rows[0].cells[colIndex];
 
-      rowNameTag.style.backgroundColor = 'transparent';
-      colNameTag.style.backgroundColor = 'transparent';
+      let currentCell = document.getElementById('current_cell').firstElementChild;
+      currentCell.innerText = `Cell : `;
+
+      rowNameTag.style.backgroundColor = 'rgb(235, 235, 235)';
+      colNameTag.style.backgroundColor = 'rgb(235, 235, 235)';
 
       
   });
 });
+
+function exportTableToExcel(){
+
+  let myWorkbook = XLSX.utils.table_to_book(document.getElementById('editableTable'), {sheet:"Sheet JS"});
+  XLSX.writeFile(myWorkbook, "RDS_fullstack_second.xlsx");
+
+}
